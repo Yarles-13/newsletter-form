53b209302b524f41e02e137e76930e6b5b29afac
Yarles <yarles@yardleys-mbp.lan>
2024-06-28 07:42:47 -0500

    I updated the dismiss button functionality

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const emailInput = document.getElementById("email");
 const submitButton = document.querySelector(".submit-button");
 const dismissbutton = document.getElementById('dismiss-button');
 
-// Submit button Javascript //
 
 function validateEmail(email) {
   return String(email)
@@ -20,20 +19,26 @@ submitButton.addEventListener("click", (e) => {
   e.preventDefault();
 
   let email = emailInput.value;
+  let invalidPTag = document.querySelector('.invalid-message');
 
   if (validateEmail(email)){
     card1.classList.add("hide");
     card2.classList.remove("hide");
     confirmationPTag.textContent = `A confirmation email has been sent to ur ${email}. Please open it and click the button to confirm your subscription `;
   } else {
-    emailInput.classList.add('input-alert');
+    emailInput.classList.add('.email-alert');
+    invalidPTag.classList.remove('hide')
   }
-
 });
+emailInput.addEventListener('input', (e) => {
+  emailInput.classList.remove('email-alert');
+  invalidPTag.classList.add('hide')
+})
 
 dismissbutton.addEventListener('click', (e) => {
   e.preventDefault()
   card2.classList.add("hide");
   card1.classList.remove("hide");
   emailInput.value = '';
+  invalidPTag.classList.add("hide");
 })
